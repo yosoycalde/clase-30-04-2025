1c8e6c61e0c3554416546de4c4f3b1271b2f8c11
yosoycalde <juancalderon2005.18@gmail.com>
2025-05-02 19:31:32 -0500

    h

---
+++ my-app/src/components/ThemeProvider.jsx
@@ -0,0 +1,19 @@
+import  { useState } from "react";
+import { ThemeContext } from "../ThemeContext";
+
+export function ThemeProvider ({ Children }) {
+    const [tema, setTema] = useState ("claro");
+
+    const toggleTema = () => {
+        setTema((prev) => (prev === "claro" ? "oscuro" : "claro"))
+
+    };
+
+    return (
+        <ThemeContext.Provider value={{tema, toggleTema}}>
+            {Children}
+        </ThemeContext.Provider>
+    )
+
+
+}
